2f2a3f54f5767e3b5e4bb9783d393a06be68a746
Yousef <yosefanwar690@gmail.com>
2023-03-22 12:25:35 +0300

    updates upon Advice.js

--- src/Advice.js
+++ src/Advice.js
@@ -1,26 +1,30 @@
 
-import React ,{useEffect, useState} from "react";
+import React ,{useState} from "react";
 
-let Advice = () =>{
-    let [count,setCount] = useState(1);
-    let [advice,setAdvice] = useState("");
-  
-    let updateAdvice = () => {
-        console.log("executed")
-        let fetchAdvice = fetch("https://api.adviceslip.com/advice")
-        .then((res) =>{
-            res.json().then(data=>{
-                setAdvice(data.slip.advice);
-            })
+const Advice = () =>{
+    const [count, setCount] = useState(1);
+    const [advice, setAdvice] = useState("");
+
+    const apiURL = "https://api.adviceslip.com/advice";
+
+    fetch(apiURL)
+        .then((response) => {
+            if (!response) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+           return response.json();
         })
-}
+        .then((data ) => {
+            return setAdvice(data.slip.advice);
+        })
+
     return(
         <div>
             <h1>Advice #{count}</h1>
             <p>{advice}</p>
         </div>
     )
-  
-  }
+}
 
   export default Advice
--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import Advice from './Advice';
+import Advice, {updateAdvice} from './Advice';
 import React , {useState,useEffect} from 'react';
 
 function App() {
+  function refreshPage() {
+    window.location.reload(false);
+  }
   return (
     <div className="container">
       <Advice/>
       <div id="pattern-divider"></div>
-      <div id='dice-container'>
+      <div id='dice-container' role= "button" onClick={refreshPage}>
         <div id="dice"></div>
       </div>
     </div>
